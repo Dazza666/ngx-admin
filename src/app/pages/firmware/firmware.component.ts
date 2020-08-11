93e0de09393afc7b3b58f9b9c555044ad69ddc7e
Darren <darren.nye@gmail.com>
2020-08-11 14:27:25 +0100

    Working firmware page - with a load of commented out code
    Just got this working, check it in before I break it, next commit will have a cleaned version

--- src/app/pages/firmware/firmware.component.ts
+++ src/app/pages/firmware/firmware.component.ts
@@ -1,84 +1,159 @@
 import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'basic-example-data',
-//   styles: [],
-//   template: `
-//   <ng2-smart-table [settings]="settings" [source]="data"></ng2-smart-table>
-//   `
-// })
-
-// export class BasicExampleDataComponent {
-
-//   settings = {
-//     columns: {
-//       id: {
-//         title: 'ID'
-//       },
-//       name: {
-//         title: 'Full Name'
-//       },
-//       username: {
-//         title: 'User Name'
-//       },
-//       email: {
-//         title: 'Email'
-//       }
-//     }
-//   };
-
-//   data = [
-//     {
-//       id: 1,
-//       name: "Leanne Graham",
-//       username: "Bret",
-//       email: "Sincere@april.biz"
-//     },
-//     // ... other rows here
-//     {
-//       id: 11,
-//       name: "Nicholas DuBuque",
-//       username: "Nicholas.Stanton",
-//       email: "Rey.Padberg@rosamond.biz"
-//     }
-//   ];
-// }
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { catchError, share, take, map } from 'rxjs/operators';
+import { NbAuthService, NbAuthToken } from '@nebular/auth';
+import { AngularFireDatabase, AngularFireObject, AngularFireList } from '@angular/fire/database';
 
 @Component({
   selector: 'ngx-dashboard',
   templateUrl: './firmware.component.html',
 })
 export class FirmwareComponent {
+
+  itemsRefList: AngularFireList<any>;
+  itemsList: Observable<any[]>;
+
+  itemRefObject: AngularFireObject<any>;
+  itemsObjects: Observable<any>;
+  userToken$: Observable<NbAuthToken>;
+  isAuthenticated$: Observable<boolean>;
+
+  constructor(
+    private authService: NbAuthService,
+    private router: Router,
+    private route: ActivatedRoute,
+    db: AngularFireDatabase,
+  ) {
+    this.userToken$ = this.authService.onTokenChange();
+    this.isAuthenticated$ = this.authService.isAuthenticated();
+    //this.items = db.object('/SOFTWARE/embedded/').valueChanges();
+    this.itemRefObject = db.object('/SOFTWARE/embedded/');
+
+    this.itemRefObject.snapshotChanges().subscribe(action => {
+      console.log(action.type);
+      console.log(action.key)
+      console.log(action.payload.val())
+    });
+    this.itemsObjects = this.itemRefObject.valueChanges();
+
+    this.itemsRefList = db.list('/SOFTWARE/embedded/ais');
+
+    this.itemsList = this.itemsRefList.snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({
+          product: c.payload.key,
+          firmware: c.payload.child('latest').val(),
+          ref: c.payload.ref,
+        }))
+      )
+    );
+  }
+
+  addItem(newName: string) {
+    this.itemsRefList.push({ latest: newName });
+  }
+  updateItem(key: string, newText: string) {
+    this.itemsRefList.update(key, { latest: newText });
+  }
+
+  logout() {
+    this.authService.logout('password');
+    this.router.navigate(['../user'], { relativeTo: this.route });
+  }
+
+  login() {
+    this.router.navigate(['../login'], { relativeTo: this.route });
+  }
+
+  resetPassword() {
+    this.router.navigate(['../reset-password'], { relativeTo: this.route });
+  }
+
+  // When the user wants to delete a entry, we ask them, but regardless what they say we will reject the promise.
+  // Why? Because we are using firebase as our datasource, but the table only works with a local datasource, we map the firebase object to a local array.
+  // To elaborate, when we update the reference the data will be sent back to firebase, which will in turn send the data back to us, which in turn will cause the local data
+  // Store to be updated and the data will refresh. So its pointless, and indeed dangerous in case the firebase write fails but looks like it succeeded
+  onDeleteConfirm(event): void {
+    if (window.confirm('WARNING! If you delete this entry, it may impact other services (e.g. AIS Mobile apps) that use this data. Are you certain?')) {
+      event.data.ref.remove();
+      //intentional, see above
+      event.confirm.reject();
+    } else {
+      event.confirm.reject();
+    }
+  }
+
+  // When the user wants to confirm a edit, we ask them, but regardless what they say we will reject the promise.
+  // Why? Because we are using firebase as our datasource, but the table only works with a local datasource, we map the firebase object to a local array.
+  // To elaborate, when we update the reference the data will be sent back to firebase, which will in turn send the data back to us, which in turn will cause the local data
+  // Store to be updated and the data will refresh. So its pointless, and indeed dangerous in case the firebase write fails but looks like it succeeded
+  onEditConfirm(event): void {
+    if (window.confirm('Are you sure what you want to make this edit?')) {
+      event.newData.ref.update({latest: event.newData.firmware});
+      //intentional, see above
+      event.confirm.reject();
+    } else {
+      event.confirm.reject();
+    }
+  }
+
+  // When the user wants to add a entry, we ask them, but regardless what they say we will reject the promise.
+  // Why? Because we are using firebase as our datasource, but the table only works with a local datasource, we map the firebase object to a local array.
+  // To elaborate, when we update the reference the data will be sent back to firebase, which will in turn send the data back to us, which in turn will cause the local data
+  // Store to be updated and the data will refresh. So its pointless, and indeed dangerous in case the firebase write fails but looks like it succeeded
+  onCreateConfirm(event): void {
+    if (window.confirm('Are you sure what you want to add this product?')) {
+      this.itemsRefList.update(event.newData.product, { latest: event.newData.firmware });
+      //event.newData.ref.update({latest: event.newData.firmware});
+      //intentional, see above
+      event.confirm.reject();
+    } else {
+      event.confirm.reject();
+    }
+  }
+
+  
+
   settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+      confirmCreate: true,
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+      confirmSave: true,
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
     columns: {
-      id: {
-        title: 'ID',
+      product: {
+        title: 'Product',
+        editable: false,
       },
-      name: {
-        title: 'Full Name',
-      },
-      username: {
-        title: 'User Name',
-      },
-      email: {
-        title: 'Email',
+      firmware: {
+        title: 'Firmware',
       },
     },
   };
 
+
+  //{ "ATB1": { "latest": "01.3.00" }, "CB2": { "latest": "01.3.00" }, "ata100": { "latest": "01.0.00" } }
+
   data = [
     {
-      id: 1,
-      name: 'Leanne Graham',
-      username: 'Bret',
-      email: 'Sincere@april.biz',
+      product: 'Leanne Graham',
+      firmware: 'Bret',
     },
-    // ... other rows here
     {
-      id: 11,
-      name: 'Nicholas DuBuque',
-      username: 'Nicholas.Stanton',
-      email: 'Rey.Padberg@rosamond.biz',
+      product: 'Nicholas DuBuque',
+      firmware: 'Nicholas.Stanton',
     },
   ];
 }
