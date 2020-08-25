799e9cc76ff8d6838fe1ff6ed3390924b00b6772
Darren <darren.nye@gmail.com>
2020-08-25 13:38:22 +0100

    have mmsi search mode working
    horrible, but flatmap and a firebase query were the key!

--- src/app/pages/ais/aisConfigs/aisConfigs.component.ts
+++ src/app/pages/ais/aisConfigs/aisConfigs.component.ts
@@ -1,6 +1,6 @@
 import { Component, ɵConsole } from '@angular/core';
 import { Observable, of, Subscription, combineLatest, forkJoin, pipe, from } from 'rxjs';
-import { map, scan, switchMap, mergeMap, mapTo, toArray } from 'rxjs/operators';
+import { map, scan, switchMap, mergeMap, mapTo, toArray, merge, flatMap } from 'rxjs/operators';
 import { AngularFireDatabase, AngularFireList, AngularFireAction } from '@angular/fire/database';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { AisConfigurationItem } from '../aisConfigurations/aisConfigurationItem';
@@ -16,35 +16,42 @@ import { debug } from 'console';
 export class AisConfigsComponent {
   tableDataLoading = true;
   userIds$: Observable<any[]>;
+  mmsi$: Observable<any[]>;
   userConfigs$: Observable<Observable<AisConfigurationItem[]>[]>
   users$: BehaviorSubject<string | null>;
+  mmsis$: Subject<string | null>;
   resultsLimit = 5;
+  database: AngularFireDatabase;
 
   constructor(
     db: AngularFireDatabase,
   ) {
+    this.database = db;
     this.users$ = new BehaviorSubject(null);
+    this.mmsis$ = new BehaviorSubject(null);
+    //setup default search method, which is email
+    this.setupUserEmailSearch();
+  }
+
+  setupUserEmailSearch() {
+    //setup what happens when data is entered into the email search box
     this.userIds$ = this.users$.switchMap(email =>
-      db.list('/USERS/', ref => {
+      this.database.list('/USERS/', ref => {
         //email = "darr"
-        let temp = email ? ref.orderByChild('EMAIL').startAt("darr").endAt("darr" + "\uf8ff").limitToFirst(this.resultsLimit) : ref.limitToLast(this.resultsLimit)
+        let temp = email ? ref.orderByChild('EMAIL').startAt(email).endAt(email + "\uf8ff").limitToFirst(this.resultsLimit) : ref.limitToLast(this.resultsLimit)
         console.log('carrot');
         return temp;
       }
       ).snapshotChanges()
     );
 
+    //sets up the search to respond to changes in the users email address entry
     this.userConfigs$ = this.userIds$.pipe(
       map(userids =>
         userids.map(userid =>
-          db.list('/AIS/USERSAVEDCONFIGS/' + userid.key).snapshotChanges().pipe(
-            map(changes => {
-              console.log(changes);
-              changes.forEach(array => {
-                console.log("gakß" + array.payload.val())
-              })
-              return AisConfigurationItem.fromFirebaseList(changes)
-            }
+          this.database.list('/AIS/USERSAVEDCONFIGS/' + userid.key).snapshotChanges().pipe(
+            map(changes =>
+              AisConfigurationItem.fromFirebaseList(changes)
             )
           )
         )
@@ -52,19 +59,72 @@ export class AisConfigsComponent {
     )
   }
 
-  onKeyUpEvent(event: any) {
-    console.log(event.target.value);
+  setupUserMmsiSearch() {
+    //setup what happens when data is entered into the mmsi search box
+
+    //So, here we are taking the mmsi that has been entered by the user, (and maybe modded to return a mmsi with a length of 9) and are querying firebase for it
+    //We are registering for these changes, and then switching to this new observable, once we have the MMSI dataset we then unfurl the data through a map
+    //and get a reference to the '/USERS/' location in the database, and query it based on the key (keys are the userIds here) starting at the user id we have pulled from
+    //the mmsi child. We limit it to 1 because only one user can have a MMSI assigned to them anyway. Critically, this also means we get the payload as the user details object
+    //otherwise we get all the inner children which is no good
+    //Finally, because this is a Observable<Observable<SnapshotActions[]>> but we want a Observable<any[]>; we use a flatmap to flaten.
+    this.userIds$ = this.mmsis$.switchMap(mmsi =>
+      this.database.list('/AIS/MMSI/', ref => mmsi ? ref.orderByKey().startAt(mmsi).limitToFirst(this.resultsLimit) : ref.limitToFirst(this.resultsLimit)
+      ).snapshotChanges()
+    ).switchMap(mmsiDataSet =>
+      mmsiDataSet.map(mmsiData => this.database.list('/USERS/', ref =>
+        ref.orderByKey().startAt(mmsiData.payload.child('userid').val()).limitToFirst(1)
+      ).snapshotChanges())
+    ).pipe(flatMap(listObservable => listObservable));
+
+    //sets up the search 
+    this.userConfigs$ = this.userIds$.pipe(
+      map(userids =>
+        userids.map(userid => {
+          console.log("goop " + userid.key)
+          return this.database.list('/AIS/USERSAVEDCONFIGS/' + userid.key).snapshotChanges().pipe(
+            map(changes =>
+              AisConfigurationItem.fromFirebaseList(changes)
+            )
+          )
+        })
+      )
+    )
+  }
+
+  onKeyUpEventMmsi(event: any) {
+    console.log("mmsi" + event.target.value);
     //Only start when we have 4 or more chars
     if (event.target.value.length >= 4) {
-      this.filterBy(event.target.value);
+      this.resultsLimit = 1;
+      let modded = event.target.value.padEnd(9, "0");
+      this.setupUserMmsiSearch();
+      this.filterByMmsi(modded);
     }
     else if (event.target.value.length == 0) {
-      this.filterBy(null);
+      this.filterByMmsi(null);
     }
   }
 
-  filterBy(email: string | null) {
+  onKeyUpEventEmail(event: any) {
+    console.log(event.target.value);
+    //Only start when we have 4 or more chars
+    if (event.target.value.length >= 4) {
+      this.resultsLimit = 5;
+      this.setupUserEmailSearch();
+      this.filterByEmail(event.target.value);
+    }
+    else if (event.target.value.length == 0) {
+      this.filterByEmail(null);
+    }
+  }
+
+  filterByEmail(email: string | null) {
     this.users$.next(email);
+  }
+
+  filterByMmsi(mmsi: string | null) {
+    this.mmsis$.next(mmsi);
   }
 
   stopLoading() {
